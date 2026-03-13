618cadfe2b57c70766f067a8be6ce461502e6749
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2026-03-12 21:52:05 -0300

    style: agregar hoja de estilos para el componente EventItem

--- src/components/Events/components/EventItems/index.jsx
+++ src/components/Events/components/EventItems/index.jsx
@@ -1,3 +1,5 @@
+import './style.css';
+
 const Eventitems = ({info, id,name,image,onEventClick})=>{
 
 const handleSeeMoreClick = (evt)=>{
@@ -5,11 +7,14 @@ const handleSeeMoreClick = (evt)=>{
     onEventClick(id);
 }
     return(
-        <div>
+        <div className="event-item-container" >
             <img src={image} alt={name} width={200} height={200}/>
-            <h4>{name}</h4>
-            <p>{info}</p>
-            <button onClick={handleSeeMoreClick}>Ver mas</button>
+            <div className="event-info-container" >
+                <h4 className="event-name" >{name}</h4>
+
+                <p className="event-info" >{info}</p>
+                <button className="see-more-btn" onClick={handleSeeMoreClick}>Ver mas</button>
+            </div>
         </div>
     );
 };
